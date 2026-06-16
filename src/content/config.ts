3e094e3f5fdd1da429c83cf7d3ffd89343479530
Noy <59097122+noyyyy@users.noreply.github.com>
2026-06-16 21:03:33 +0800

    chore: remove description

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from "astro:content";
 const post = defineCollection({
 	schema: {
 		title: z.string().max(60),
-		description: z.string().min(50).max(160),
+		description: z.string().min(0).max(160),
 		publishDate: z.string().transform((str) => new Date(str)),
 		tags: z.array(z.string()).default([]),
 	},
